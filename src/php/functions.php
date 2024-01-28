e0e0bc790fdae3f566c443f990f59b1ba645f1d1
Angela <antunezangela89@gmail.com>
2024-01-28 22:10:28 +0100

    Use PHP Intelephense

--- src/php/functions.php
+++ src/php/functions.php
@@ -1,10 +1,11 @@
 <?php
-    function randomWord($array){
-        $currentWord = $_SESSION["currentWord"];
-        if ($currentWord) {
-            return $currentWord;
-        } 
-        $currentWord = $array[array_rand($array)];
-        $_SESSION["currentWord"] = $currentWord;
+function randomWord($array)
+{
+    $currentWord = $_SESSION["currentWord"];
+    if ($currentWord) {
         return $currentWord;
     }
+    $currentWord = $array[array_rand($array)];
+    $_SESSION["currentWord"] = $currentWord;
+    return $currentWord;
+}
